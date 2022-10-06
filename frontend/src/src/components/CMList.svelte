9d5220cc5b0fdc32a54fb44d3004e8bb598fe931
Arthurllyr <arthur@leluyer.org>
2022-10-06 13:55:58 +0200

    consult pdf button

--- frontend/src/src/components/CMList.svelte
+++ frontend/src/src/components/CMList.svelte
@@ -240,8 +240,7 @@
                                 {@html $popupInformation}
                             </table>
                             <!-- modifier localhost -->
-                            <a href="http://localhost:7000/images/pdf/st_hip_watermark.pdf" target="_blank" >consulter le pdf</a>
-
+                            <button class="btn btn-success" onclick=" window.open('http://localhost:7000/images/pdf/st_hip_watermark.pdf','_blank')">consulter le pdf</button>
                         </div>
                     </div>
             </div>
