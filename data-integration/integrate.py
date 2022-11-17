3bffdf360074e95506062fa255c1e1131aa362d7
Cédric M. Serugendo <cedric.serugendo@crem.ch>
2022-11-18 00:30:29 +0100

    update

--- data-integration/integrate.py
+++ data-integration/integrate.py
@@ -1,6 +1,7 @@
 import fake_data
 from legends import post_legend
 from utilities import get_engine
+import regbl_data 
 
 # create engine
 engine = get_engine()
@@ -11,3 +12,6 @@ post_legend(engine=engine)
 # post the data
 geo_dataframe = fake_data.read_data()
 fake_data.post_data(data=geo_dataframe, engine_=engine)
+
+# regbl
+regbl_data.main()
--- data-integration/paths.py
+++ data-integration/paths.py
@@ -5,5 +5,7 @@ CURRENT_DIR = dirname(abspath(__file__))
 DATA_DIR = join(CURRENT_DIR, "data")
 FAKE_DATA_DIR = join(DATA_DIR, "fake_data")
 REGBL_DATA_DIR = join(DATA_DIR, "regbl_data")
+FOOTPRINT_DATA_DIR = join(DATA_DIR, "footprint_data")
 
 INPUTS_DIR = join(CURRENT_DIR, "inputs")
+CANTON_FOOTPRINT_CSVS_DIR = join(INPUTS_DIR, "canton_footprint")
