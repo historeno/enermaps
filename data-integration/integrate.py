3a5d23e3f079270ccea2034df4b4e82e8def1f5b
Cédric M. Serugendo <cedric.serugendo@crem.ch>
2022-11-17 10:55:41 +0100

    change data.py module name (add new file)

---
+++ data-integration/integrate.py
@@ -0,0 +1,13 @@
+import fake_data
+from legends import post_legend
+from utilities import get_engine
+
+# create engine
+engine = get_engine()
+
+# post legends
+post_legend(engine=engine)
+
+# post the data
+geo_dataframe = fake_data.read_data()
+fake_data.post_data(data=geo_dataframe, engine_=engine)
